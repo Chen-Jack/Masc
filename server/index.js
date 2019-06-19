69e831ce7d89f55787ef7f1b1d0a229ac9ae7460
Jack Chen <Jack.Chen@qubit.com>
2019-06-18 22:03:47 -0400

    Created database files for sqlite3

--- server/index.js
+++ server/index.js
@@ -3,23 +3,33 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
 const PORT = 3001
-
+const sql = require('./db.js')
 
 // Middleware
 app.use(cors())
 app.use(bodyParser.json())
 
-
 // app.use(express.static('./client/dist'))
 
 app.get('/', (req, res) => {
   res.send('Home')
-  // res.sendFile('./client/dist/index.html')
+})
+
+app.get('write', (req, res) => {
+  sql.query('')
+  res.status(200)
+})
+
+app.get('read', (req, res) => {
+  sql.run('SELECT * FROM posts', () => {
+    res.status(200)
+  })
 })
 
 app.post('/signup', (req, res) => {
   console.log(req.body)
-  if (true) {
+  const formIsValid = true
+  if (formIsValid) {
     res.status(200)
   } else {
     const err = 'There was an error'
@@ -31,6 +41,5 @@ app.get('*', (req, res) => {
   res.send('404 ')
   // res.sendFile('./client/dist/index.html')
 })
-
 
 const server = app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
