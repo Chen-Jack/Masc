4f48abb5cb63330d7f70b40dc2cd687c2a900fc3
Jack Chen <Jack.Chen@qubit.com>
2019-06-19 22:25:31 -0400

    Requiring JWT and JWT secret
    Changed gitignore to exclude secrets file

--- server/index.js
+++ server/index.js
@@ -6,14 +6,15 @@ const PORT = 3010
 const db = require('./db')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
-
+const jwt = require('jsonwebtoken')
+const { jwtSecret } = require('./secret')
 // Middleware
 app.use(cors())
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }))
-// app.use(bodyParser.json())
+// app.use(bodyParserls.json())
 
 // app.use(express.static('./client/dist'))
 
